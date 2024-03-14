e19e9f4ae6c483fc6bf29428d6fb5c0dca3fd29f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-03-14 13:10:53 -0400

    chore: Add the exam/questions/question_one.py file with TODO markers.

---
+++ exam/questions/question_one.py
@@ -0,0 +1,114 @@
+"""Question One: Algorithm Analysis Executable Examination."""
+
+# TODO: The imports in the following source code block no longer
+# adhere to the industry best practices for Python source code.
+# You must reorganize and/or add the imports so that they adhere
+# to the industry best practices for Python source code.
+
+from typing import List, Union
+
+# Introduction: Read This First! {{{
+
+# Keep in mind these considerations as you implement the required functions:
+
+# --> You must implement Python functions to complete each of these steps,
+# bearing in mind that one defective function may break another function.
+
+# --> Your source code must adhere to industry best practices in, for instance,
+# source code formatting, variable naming, and documentation.
+
+# --> You may refer to the checks that are specified in the exam/gatorgrade.yml file
+# in this GitHub repository for the configuration and name of each tool used
+# to analyze the code inside of this file.
+
+# }}}
+
+# Part (a) {{{
+
+# Instructions:
+# Implement the following function that inputs the contents of a file
+# so that it adheres to all aspects of the following specification.
+
+# Function specification:
+# The function find_minimum_value should:
+# - Take a list of lists of integers, called matrix, as its parameter
+# - Return an integer that represents the minimum value in the matrix
+
+# TODO: If the function is called with an invalid input (e.g., an empty
+# matrix), it should return None.
+
+# TODO: This function may not not have all of the correct type annotations for
+# certain variables. You must add all of any needed type annotations
+# so that the function and any code that uses it passes the type checker.
+
+# TODO: This function may not have a docstring and thus it may not adhere
+# to industry best practices for Python source code. You may need to add a docstring
+# so that this function is correctly documented by an algorithm engineer using it.
+
+
+def find_minimum_value(matrix: List[List[int]]) -> Union[int, None]:
+    """Return the minimum value in the provided matrix."""
+    return None
+
+
+# }}}
+
+# Part (b) {{{
+
+# Instructions:
+# Implement the following function that inputs the contents of a file
+# so that it adheres to all aspects of the following specification.
+
+# Function specification:
+# The function find_maximum_value should:
+# - Take a list of lists of integers, called matrix, as its parameter
+# - Return an integer that represents the maximum value in the matrix
+
+# TODO: If the function is called with an invalid input (e.g., an empty
+# matrix), it should return None.
+
+# TODO: This function may not not have all of the correct type annotations for
+# certain variables. You must add all of any needed type annotations
+# so that the function and any code that uses it passes the type checker.
+
+# TODO: This function may not have a docstring and thus it may not adhere
+# to industry best practices for Python source code. You may need to add a docstring
+# so that this function is correctly documented by an algorithm engineer using it.
+
+
+def find_maximum_value(matrix: List[List[int]]) -> Union[int, None]:
+    return None
+
+
+# }}}
+
+
+# Part (c) {{{
+
+# Instructions:
+# Implement the following function that inputs the contents of a file
+# so that it adheres to all aspects of the following specification.
+
+# Function specification:
+# The function find_average_value should:
+# - Take a list of lists of integers, called matrix, as its parameter
+# - Return a floating-point number that represents the average value of all numbers in the matrix
+
+# TODO: If the function is called with an invalid input (e.g., an empty
+# matrix), it should return None.
+
+# TODO: This function may not not have all of the correct type annotations for
+# certain variables. You must add all of any needed type annotations
+# so that the function and any code that uses it passes the type checker.
+
+# TODO: This function may not have a docstring and thus it may not adhere
+# to industry best practices for Python source code. You may need to add a docstring
+# so that this function is correctly documented by an algorithm engineer using it.
+
+
+def find_average_value(matrix):
+    """Find the average value in the provided matrix."""
+    return None
+
+
+# }}}
